9a86c20c23985a58bcc6ea7eff29aeade780f06e
sawyer32 <jesper.hunesjo@ufind.se>
2023-02-15 10:24:32 +0100

    .

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Layout from "./pages/Layout";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
-import { ProtectedRoute } from "./Auth/ProtectedRoute";
-import { ProtectedLayout } from "./components/ProtectedLayout";
-import { ProfilePage } from "./components/ProfilePage";
 import PrivateRoutes from "./Auth/PrivateRoutes";
 
 function App() {
